1fe519737964c1904232194fb2ff920031b2951b
soranko <kalle.soranko@gmail.com>
2017-07-10 14:34:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring Boot Demo for Fujitsu
+- Train API can be found from: https://rata.digitraffic.fi/api/v1/all-trains
---
+++ src/main/java/com/soranko/JunatApplication.java
@@ -0,0 +1,12 @@
+package com.soranko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunatApplication.class, args);
+	}
+}
